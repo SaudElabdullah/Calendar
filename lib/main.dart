8bc69e4c3eafb1225745e9d3e687b57190ce6f05
SAWD ALABDALLAH <Soa142812222@gmail.com>
2021-12-29 11:50:57 +0300

    Some customization

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 void main() {
-  return runApp(CalendarApp());
+  return runApp(const CalendarApp());
 }
 
 /// The app which hosts the home page which contains the calendar on it.
 class CalendarApp extends StatelessWidget {
+  const CalendarApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(title: 'Calendar Demo', home: MyHomePage());
@@ -24,56 +26,52 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   double height = 900.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-          child: Container(
-            height: height,
-            child: SfCalendar(
-              view: CalendarView.month,
-              dataSource: MeetingDataSource(_getDataSource()),
-              onTap: (CalendarTapDetails details) {
+      body: SafeArea(
+        child: TweenAnimationBuilder<double>(
+          tween: Tween<double>(begin: 900, end: height),
+          duration: const Duration(seconds: 1),
+          builder: (BuildContext context, double size, Widget? child) {
+            return SizedBox(
+              height: size,
+              child: SfCalendar(
+                view: CalendarView.month,
+                dataSource: MeetingDataSource(_getDataSource()),
+                onTap: (CalendarTapDetails details) {
                   setState(() {
-                    height = 500;
+                    height = height == 900 ? 500 : 900;
                   });
-              },
-              // onTap: (){
-              //   setState(() {
-              //     height = 500;
-              //   });
-              // },
-              // by default the month appointment display mode set as Indicator, we can
-              // change the display mode as appointment using the appointment display
-              // mode property
-              monthViewSettings: const MonthViewSettings(
-                  appointmentDisplayMode: MonthAppointmentDisplayMode.appointment),
-            ),
-          ),
-        ));
+                },
+                monthViewSettings: const MonthViewSettings(
+                  appointmentDisplayMode:
+                      MonthAppointmentDisplayMode.appointment,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 
   List<Meeting> _getDataSource() {
     final List<Meeting> meetings = <Meeting>[];
     final DateTime today = DateTime.now();
     final DateTime startTime =
-    DateTime(today.year, today.month, today.day, 9, 0, 0);
+        DateTime(today.year, today.month, today.day, 9, 0, 0);
     final DateTime endTime = startTime.add(const Duration(hours: 2));
-    meetings.add(Meeting(
-        'Conference', startTime, endTime, const Color(0xFF0F8644), false));
+    meetings.add(
+      Meeting('Conference',Icon(Icons.link), startTime, endTime, const Color(0xFF0F8644), false),
+    );
     return meetings;
   }
 }
 
-/// An object to set the appointment collection data source to calendar, which
-/// used to map the custom appointment data to the calendar appointment, and
-/// allows to add, remove or reset the appointment collection.
 class MeetingDataSource extends CalendarDataSource {
-  /// Creates a meeting data source, which used to set the appointment
-  /// collection to the calendar
   MeetingDataSource(List<Meeting> source) {
     appointments = source;
   }
@@ -114,24 +112,19 @@ class MeetingDataSource extends CalendarDataSource {
   }
 }
 
-/// Custom business object class which contains properties to hold the detailed
-/// information about the event data which will be rendered in calendar.
 class Meeting {
-  /// Creates a meeting class with required details.
-  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay);
+  Meeting(this.eventName, this.icon, this.from, this.to, this.background,
+      this.isAllDay);
 
-  /// Event name which is equivalent to subject property of [Appointment].
   String eventName;
 
-  /// From which is equivalent to start time property of [Appointment].
+  Icon icon;
+
   DateTime from;
 
-  /// To which is equivalent to end time property of [Appointment].
   DateTime to;
 
-  /// Background which is equivalent to color property of [Appointment].
   Color background;
 
-  /// IsAllDay which is equivalent to isAllDay property of [Appointment].
   bool isAllDay;
 }
